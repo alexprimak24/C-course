b4108bccc2233e4f1611304779e974fc62cd3334
alexprimak24 <aprimak43@gmail.com>
2024-11-01 01:58:46 +0200

    some basics pt1

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,9 +1,35 @@
 #include <stdio.h>
 
-int main(){
-    
+int main(){ 
+
+    // \n new line
+    // \t tab   
     printf("I like pizza!\n");
     printf("It's really good\n");
+    printf("\"I like pizza\" - Abraham Lincoln probably\n");
+
+    //variables = Allocated space in memory to store the value
+    //we refer to a variable's name to access the stored value.
+    //That variable now behaves as if it was the value it contains
+    //BUT we need to declare what type of data we are storing
+    
+    
+    int x; //declaration
+    x = 123; //initialization
+    int y = 321; //declaration and initialization
+
+    int age = 21; //integer
+    float gpa = 2.05; //floating points number
+    char grade = 'C'; //single character
+    char name[] = "Bro"; // array of characters
+    
+    //s - string, d - decimal....
+    printf("Hello %s\n",name);
+    printf("You are %d years old\n", age);
+    printf("Your average grade is %c\n",grade);
+    printf("Your gpa is %f\n",gpa);
     return 0;
+
+
     
 }
