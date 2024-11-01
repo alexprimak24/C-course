5e2ed3385ad8f70086d638e9520cbafd8769cef6
alexprimak24 <aprimak43@gmail.com>
2024-11-01 14:37:34 +0200

    basics pt2

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(){ 
 
@@ -28,8 +29,33 @@ int main(){
     printf("You are %d years old\n", age);
     printf("Your average grade is %c\n",grade);
     printf("Your gpa is %f\n",gpa);
+
+    char a = "C"; //single character %c
+    char b[] = "Bro"; //array of characters %s
+
+    float c = 3.141592; //4 bytes (32 bits of precision) 6 - 7 digits %f
+    //double d = 3.141592653589739 //8 bytes (64 bits of precision) 15 - 16 digits %lf
+
+    //bool e = true; //1 bytes (true or false) %d
+
+    //char f = 100; //1 byte (-128 to +127) %d or %c
+    //unsigned char g = 255; //1 byte (0 to +255) %d or %c
+
+    //In case of overflow we are back to minimal value -32.768 in our case
+    // short int h = 32767; //2 bytes (-32.768 to +32.767) %d
+    //unsigned short int i = 65535; //2 bytes (0 to +65.535) %d
+    //short h = 32767 - usually just write it that way
+    
+    //for int there is no decimal portion
+    //actually the whole declaration is: long int j = 21.......
+    int j = 2147483647; //4 bytes (-2,147,483,648 to +2,147,483,647) %d
+    unsigned int k = 4294967295L; //4 bytes (0 to +4,294.967,295) %u
+
+    long long int l = 922333323424234; //8 bytes (-9 quintillion to +9 quintillion)
+    unsigned long long int m = 184423423423; //8 bytes (0 to +18 quintillion)
+
     return 0;
 
-
+    
     
 }
